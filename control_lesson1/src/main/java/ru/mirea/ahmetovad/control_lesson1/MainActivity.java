cd58f577192e9c2dab08edd232ac6727f4d86790
demon <aahmetov8897@gmail.com>
2026-02-17 17:57:52 +0300

    lesson1

---
+++ control_lesson1/src/main/java/ru/mirea/ahmetovad/control_lesson1/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.mirea.ahmetovad.control_lesson1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView titleTextView;
+    private Button searchButton;
+    private EditText searchEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        titleTextView = findViewById(R.id.titleTextView);
+        searchButton = findViewById(R.id.button);
+        searchEditText = findViewById(R.id.searchEditText);
+
+        titleTextView.setText("Поисковик готов к работе");
+        searchButton.setText("Найти");
+        searchEditText.setText("");
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,5 @@ dependencyResolutionManagement {
 rootProject.name = "Lesson1"
 include(":app")
 include(":lauouttype")
+include(":control_lesson1")
+include(":buttoncliker")
